da7803ec10dbfbb9e2b507f2d1c02c61f9d960a2
abourouh <abourouhmustapha@gmail.com>
2018-03-26 17:33:40 +0100

    fix filter cors : origin headers and authorizations

--- bq/src/main/java/com/own/bq/security/WebSecurityConfig.java
+++ bq/src/main/java/com/own/bq/security/WebSecurityConfig.java
@@ -1,15 +1,21 @@
 package com.own.bq.security;
 
+import javax.servlet.Filter;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import com.own.bq.security.filers.FilterCors;
 
 @Configuration
 @EnableWebSecurity
@@ -17,10 +23,16 @@ import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	private static final Logger LOG = LoggerFactory.getLogger(WebSecurityConfig.class);
-
+	
+	@Bean
+	Filter corsFilter() {
+		return new FilterCors();
+	}
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 
+		http.authorizeRequests().antMatchers(HttpMethod.OPTIONS).permitAll();
 		
 		// skip swagger
 		http.authorizeRequests().antMatchers("/rest/v2/api-docs", 
@@ -28,9 +40,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 				"/configuration/security", "/rest/swagger-ui.html**", "/rest/webjars/**").permitAll();
 		
 		
+		
 		http.csrf().disable().authorizeRequests().antMatchers("/**").hasRole("ADMIN").and().
 		httpBasic().and().headers().frameOptions()
 				.disable();
+		
+		http.addFilterBefore(corsFilter(), BasicAuthenticationFilter.class);
 
 
 	}
--- bq/src/main/java/com/own/bq/security/filers/FilterCors.java
+++ bq/src/main/java/com/own/bq/security/filers/FilterCors.java
@@ -8,13 +8,19 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 
 public class FilterCors implements Filter {
 
 	private static final Logger LOG = LoggerFactory.getLogger(FilterCors.class);
+
+	@Value("${allowOrigin:*}")
+	private String allowOrigin;
 
 	@Override
 	public void destroy() {
@@ -22,10 +28,22 @@ public class FilterCors implements Filter {
 	}
 
 	@Override
-	public void doFilter(ServletRequest arg0, ServletResponse resp, FilterChain arg2)
+	public void doFilter(ServletRequest request, ServletResponse resp, FilterChain arg2)
 			throws IOException, ServletException {
-		LOG.info("------------------------pass Filter----------------------------");
-		arg2.doFilter(arg0, resp);
+		HttpServletResponse response = (HttpServletResponse) resp;
+		response.setHeader("Access-Control-Allow-Origin", allowOrigin);
+		response.setHeader("Access-Control-Allow-Credentials", "true");
+		response.setHeader("Access-Control-Allow-Methods",
+				"ACL, CANCELUPLOAD, CHECKIN, CHECKOUT, COPY, DELETE, GET, HEAD, LOCK, MKCALENDAR, MKCOL, MOVE, OPTIONS, POST, PROPFIND, PROPPATCH, PUT, REPORT, SEARCH, UNCHECKOUT, UNLOCK, UPDATE, VERSION-CONTROL");
+		response.setHeader("Access-Control-Max-Age", "3600");
+		response.setHeader("Access-Control-Allow-Headers",
+				"Origin, X-Requested-With, Content-Type, Accept, Key, Authorization");
+
+		if ("OPTIONS".equalsIgnoreCase(((HttpServletRequest) request).getMethod())) {
+			response.setStatus(HttpServletResponse.SC_OK);
+		} else {
+			arg2.doFilter(request, response);
+		}
 
 	}
 
